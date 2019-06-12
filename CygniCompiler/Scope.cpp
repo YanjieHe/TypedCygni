79600607ea8d531c1ab26bdbd9e7bc39b7034b58
Yanjie He <heyanjie0@outlook.com>
2019-06-12 00:40:26 -0400

    add some comments on scope

--- CygniCompiler/Scope.cpp
+++ CygniCompiler/Scope.cpp
@@ -1,43 +1,58 @@
 #include "Scope.hpp"
 #include "Exception.hpp"
 
-Scope::Scope(Ptr<Scope> parent) : parent{std::move(parent)}
+Scope::Scope(Ptr<Scope> parent)
+        : parent{std::move(parent)}
 {
 
 }
 
 Optional<Any> Scope::Lookup(const String &name, const String &key)
 {
-    if (values.find(name) != values.end() && values[name].find(key) != values[name].end())
+    auto tryLookupParent = [this](const String &name, const String &key)
     {
-        return Optional<Any>(values[name][key]);
-    }
-    else if (parent)
+        if (parent)
+        {
+            return parent->Lookup(name, key);
+        }
+        else
+        {
+            return Optional<Any>();
+        }
+    };
+    if (values.find(name) != values.end()) // found name
     {
-        return parent->Lookup(name, key);
+        if (values[name].find(key) != values[name].end()) // found key
+        {
+            return Optional<Any>(values[name][key]);
+        }
+        else
+        {
+            return tryLookupParent(name, key);
+        }
     }
     else
     {
-        return Optional<Any>();
+        return tryLookupParent(name, key);
     }
 }
 
 void Scope::Put(const String &name, const String &key, const Any &value)
 {
-    if (values.find(name) != values.end())
+    if (values.find(name) != values.end()) // found name
     {
-        if (values[name].find(key) != values[name].end())
+        if (values[name].find(key) != values[name].end()) // found key
         {
             throw ArgumentException("ArgumentException: An item with the same key has already been added.");
         }
         else
         {
-            values[name].insert({key, value});
+            values[name].insert({key, value}); // insert value if (name, key) does not exists
         }
     }
     else
     {
-        values.insert({name, HashMap<String, Any>()});
+        values.insert({name, HashMap<String, Any>()}); // create key first
         values[name].insert({key, value});
     }
 }
